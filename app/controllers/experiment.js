f2039855bc07795ef5e5b3af18e3ac36b3ed8fa2
Naman Jain <naman-jain@outlook.com>
2020-03-29 00:46:35 +0100

    Added tests

--- app/controllers/experiment.js
+++ app/controllers/experiment.js
@@ -14,23 +14,10 @@ module.exports.addExperiment = async (ownerId, experiment) => {
     let db = await mongo.connect();
 
     experiment["_id"]["projectId"] = ObjectID(experiment["_id"]["projectId"]);
-    if (!(await validateOwner(ownerId, experiment["_id"]["projectId"]))) {
-        throw new createError(
-            401,
-            "Not Authorized to create experiment under given project",
-        );
-    }
 
-    if (!validateVariations(experiment["variations"])) {
-        throw new createError(400, "Invalid variations");
-    }
-
-    if (!(await validateStartAndEndTime(experiment))) {
-        throw new createError(
-            400,
-            "Invalid start/endtime. Please send start/endTime in Unixtimestamps (milliseconds).",
-        );
-    }
+    await validateOwner(ownerId, experiment["_id"]["projectId"]);
+    await validateStartAndEndTime(experiment);
+    validateVariations(experiment["variations"]);
 
     try {
         const response = await db
@@ -103,68 +90,75 @@ module.exports.getRunningExperimentsInTimeRange = async (
 };
 
 /**
- * Checks if the given owner owns this project
+ * Checks if the given owner owns this project. Throws error if it is not owner
  *
  * @async
  * @param {string} ownerId
  * @param {ObjectID} projectId
- *
- * @returns {Promise<boolean>}
  */
 const validateOwner = async (ownerId, projectId) => {
     let db = await mongo.connect();
+    let project = null;
     try {
-        return (
-            (await db.collection("projects").findOne({
-                ownerId,
-                _id: projectId,
-            })) !== null
-        );
+        project = await db.collection("projects").findOne({
+            ownerId,
+            _id: projectId,
+        });
     } catch (err) {
         logger.error(err);
         throw new createError(500);
     }
+    if (project === null) {
+        throw new createError(
+            401,
+            "Not Authorized to create experiment under given project",
+        );
+    }
 };
 
 /**
- * Does validation on start and end times
+ * Does validation on start and end times. Throws error if times are not avlid
  *
  * @param {Object} experiment
- * @returns {boolean}
  */
 const validateStartAndEndTime = (experiment) => {
     //Checks if start time is valid
     if (!experiment.startTime) {
         experiment.startTime = new Date().getTime();
     } else if (experiment.startTime < new Date().getTime()) {
-        return false;
+        throw new createError(
+            400,
+            "Invalid start/endtime. startTime cannot be in the past",
+        );
     }
     experiment.startTime = Timestamp.fromNumber(experiment.startTime);
 
     //Checks if endtime is valid
     if (experiment.endTime) {
-        if (experiment.endTime <= experiment.startTime) return false;
+        if (experiment.endTime <= experiment.startTime) {
+            throw new createError(
+                400,
+                "Invalid start/endtime. endtime has to be after start time",
+            );
+        }
         experiment.endTime = Timestamp.fromNumber(experiment.endTime);
     } else {
         experiment.endTime = null;
     }
-
-    return true;
 };
 
 /**
- * Checks if the given variations of an experiment are valid
+ * Checks if the given variations of an experiment are valid. Throws errors if the variations are not valid
  *
  * @param {Array<Object>} variations
- * @returns {boolean}
  */
 const validateVariations = (variations) => {
-    return (
-        Array.isArray(variations) &&
-        validateVariationsTrafficAddsUpTo1(variations) &&
-        validateVariationsHaveSameVariables(variations) &&
-        validateVariationsHaveUniqueNames(variations)
-    );
+    if (!Array.isArray(variations) || variations.length < 2) {
+        throw new createError(400, "There must be atleast 2 variations");
+    }
+    validateVariationsHaveUniqueNames(variations);
+    validateVariationsTrafficAddsUpTo1(variations);
+    validateVariationsHaveSameVariables(variations);
 };
 
 /**
@@ -177,10 +171,11 @@ const validateVariationsHaveUniqueNames = (variations) => {
     let names = {};
     for (let variation of variations) {
         let variationName = variation.variationName;
-        if (!variationName || names[variationName]) return false;
+        if (!variationName || names[variationName]) {
+            throw new createError(400, "Variations must have unique names");
+        }
         names[variation.variationName] = true;
     }
-    return true;
 };
 
 /**
@@ -189,25 +184,39 @@ const validateVariationsHaveUniqueNames = (variations) => {
  * @param {Array<Object>}
  * @returns {boolean}
  */
-const validateVariationsTrafficAddsUpTo1 = (variations) =>
-    variations.reduce(
-        (v1, v2) =>
-            (v1.normalizedTrafficAmount || 0) +
-            (v2.normalizedTrafficAmount || 0),
-    ) == 1;
+const validateVariationsTrafficAddsUpTo1 = (variations) => {
+    if (
+        variations.reduce(
+            (v1, v2) =>
+                (v1.normalizedTrafficAmount || 0) +
+                (v2.normalizedTrafficAmount || 0),
+        ) != 1
+    ) {
+        throw new createError(
+            400,
+            "Traffic of all variations must add up to 100%",
+        );
+    }
+};
 
 /**
- * All variations must have the same variables
+ * All variations must have the same variables. Throws error if this is not the case
  *
  * @param {Array} variations
  */
 const validateVariationsHaveSameVariables = (variations) => {
     //This is the variable set we are going to test against. If variables from other variations do not match, we return false.
     let testVariables = variations[0].variables;
-    if (!validateVariablesHaveUniqueNames(testVariables)) return false;
+
+    if (!Array.isArray(testVariables) || testVariables.length < 1) {
+        throw new createError(400, "There must be atleast 1 variable");
+    }
+    if (!variablesHaveUniqueNames(testVariables)) {
+        throw new createError(400, "Variables must have unique names");
+    }
 
     //Checking if every variation has same variables as testVariables
-    return variations.every(
+    let variablesAreConsistent = variations.every(
         (variation) =>
             Array.isArray(variation.variables) &&
             //Checking length of testVariables to variation's variable
@@ -221,6 +230,13 @@ const validateVariationsHaveSameVariables = (variations) => {
                 ),
             ),
     );
+
+    if (!variablesAreConsistent) {
+        throw new createError(
+            400,
+            "Variables names and types must be the same accross all variations",
+        );
+    }
 };
 
 /**
@@ -229,9 +245,8 @@ const validateVariationsHaveSameVariables = (variations) => {
  * @param {Array} variables
  * @returns {boolean}
  */
-const validateVariablesHaveUniqueNames = (variables) => {
+const variablesHaveUniqueNames = (variables) => {
     let names = {};
-    if (!Array.isArray(variables)) return false;
     for (let variable of variables) {
         let variableName = variable.variableName;
         if (!variableName || names[variableName]) return false;
---
+++ app/test/controllers/addExperiment.test.js
@@ -0,0 +1,392 @@
+let assert = require("assert");
+let mongo = require("../../db/mongodb");
+let projectController = require("../../controllers/project");
+let experimentController = require("../../controllers/experiment");
+
+let ownerId = "testOwner";
+let projectName = "testProject";
+
+beforeEach(async () => {
+    let db = await mongo.connect();
+    let collections = await db.listCollections().toArray();
+    collections.forEach(
+        async (collection) =>
+            await db.collection(collection.name).deleteMany({}),
+    );
+    await projectController.addProject(ownerId, projectName);
+});
+
+afterAll(async () => {
+    await mongo.disconnect();
+});
+
+describe("Experiment Controller", () => {
+    it("can add an experiment without start/end time", async () => {
+        let projects = await projectController.getProjectsByOwner(ownerId);
+
+        let addedExperiment = await experimentController.addExperiment(
+            ownerId,
+            mockExperiment(projects[0]["_id"]),
+        );
+
+        assert.equal(addedExperiment._id.projectId, projects[0]["_id"]);
+        assert.ok(
+            Math.abs(addedExperiment.startTime - new Date().getTime()) < 10000,
+        );
+        assert.equal(addedExperiment.endTime, null);
+    });
+
+    it("can add an experiment with only start time", async () => {
+        let projects = await projectController.getProjectsByOwner(ownerId);
+
+        let startTime = new Date().getTime() + 10000;
+        let addedExperiment = await experimentController.addExperiment(
+            ownerId,
+            mockExperiment(projects[0]["_id"], startTime),
+        );
+
+        assert.equal(addedExperiment._id.projectId, projects[0]["_id"]);
+        assert.equal(addedExperiment.startTime, startTime);
+        assert.equal(addedExperiment.endTime, null);
+    });
+
+    it("can add an experiment with only end time", async () => {
+        let projects = await projectController.getProjectsByOwner(ownerId);
+
+        let endTime = new Date().getTime() + 30000;
+        let addedExperiment = await experimentController.addExperiment(
+            ownerId,
+            mockExperiment(projects[0]["_id"], undefined, endTime),
+        );
+
+        assert.equal(addedExperiment._id.projectId, projects[0]["_id"]);
+        assert.ok(
+            Math.abs(addedExperiment.startTime - new Date().getTime()) < 10000,
+        );
+        assert.equal(addedExperiment.endTime, endTime);
+    });
+
+    it("can add an experiment with both start and end time", async () => {
+        let projects = await projectController.getProjectsByOwner(ownerId);
+
+        let startTime = new Date().getTime() + 10000;
+        let endTime = new Date().getTime() + 30000;
+        let addedExperiment = await experimentController.addExperiment(
+            ownerId,
+            mockExperiment(projects[0]["_id"], startTime, endTime),
+        );
+
+        assert.equal(addedExperiment._id.projectId, projects[0]["_id"]);
+        assert.equal(addedExperiment.startTime, startTime);
+        assert.equal(addedExperiment.endTime, endTime);
+    });
+
+    it("cannot add an experiment with start time before current time", async () => {
+        let projects = await projectController.getProjectsByOwner(ownerId);
+
+        try {
+            let startTime = new Date().getTime() - 10000;
+            await experimentController.addExperiment(
+                ownerId,
+                mockExperiment(projects[0]["_id"], startTime),
+            );
+            assert.fail();
+        } catch (err) {
+            assert.ok(err.message.includes("startTime cannot be in the past"));
+        }
+    });
+
+    it("cannot add an experiment with end time before start time", async () => {
+        let projects = await projectController.getProjectsByOwner(ownerId);
+
+        try {
+            let startTime = new Date().getTime() + 20000;
+            let endTime = new Date().getTime() + 10000;
+            await experimentController.addExperiment(
+                ownerId,
+                mockExperiment(projects[0]["_id"], startTime, endTime),
+            );
+            assert.fail();
+        } catch (err) {
+            assert.ok(
+                err.message.includes("endtime has to be after start time"),
+            );
+        }
+    });
+
+    it("cannot add an experiment with end time before current time", async () => {
+        let projects = await projectController.getProjectsByOwner(ownerId);
+
+        try {
+            let endTime = new Date().getTime() - 10000;
+            await experimentController.addExperiment(
+                ownerId,
+                mockExperiment(projects[0]["_id"], undefined, endTime),
+            );
+            assert.fail();
+        } catch (err) {
+            assert.ok(
+                err.message.includes("endtime has to be after start time"),
+            );
+        }
+    });
+
+    it("cannot add an experiment with less than 2 variations", async () => {
+        let projects = await projectController.getProjectsByOwner(ownerId);
+
+        try {
+            await experimentController.addExperiment(
+                ownerId,
+                mockExperiment(projects[0]["_id"], undefined, undefined, [
+                    {
+                        variationName: "variation1",
+                    },
+                ]),
+            );
+            assert.fail();
+        } catch (err) {
+            assert.ok(
+                err.message.includes("There must be atleast 2 variations"),
+            );
+        }
+    });
+
+    it("cannot add an experiment with duplicate variation names", async () => {
+        let projects = await projectController.getProjectsByOwner(ownerId);
+
+        try {
+            await experimentController.addExperiment(
+                ownerId,
+                mockExperiment(projects[0]["_id"], undefined, undefined, [
+                    {
+                        variationName: "variation1",
+                    },
+                    {
+                        variationName: "variation1",
+                    },
+                ]),
+            );
+            assert.fail();
+        } catch (err) {
+            assert.ok(
+                err.message.includes("Variations must have unique names"),
+            );
+        }
+    });
+
+    it("cannot add an experiment with undefined variation name", async () => {
+        let projects = await projectController.getProjectsByOwner(ownerId);
+
+        try {
+            await experimentController.addExperiment(
+                ownerId,
+                mockExperiment(projects[0]["_id"], undefined, undefined, [
+                    {},
+                    {
+                        variationName: "variation1",
+                    },
+                ]),
+            );
+            assert.fail();
+        } catch (err) {
+            assert.ok(
+                err.message.includes("Variations must have unique names"),
+            );
+        }
+    });
+
+    it("cannot add an experiment with traffic less than 100%", async () => {
+        let projects = await projectController.getProjectsByOwner(ownerId);
+
+        try {
+            await experimentController.addExperiment(
+                ownerId,
+                mockExperiment(projects[0]["_id"], undefined, undefined, [
+                    {
+                        variationName: "variation2",
+                        normalizedTrafficAmount: 0.4,
+                    },
+                    {
+                        variationName: "variation1",
+                        normalizedTrafficAmount: 0.5,
+                    },
+                ]),
+            );
+            assert.fail();
+        } catch (err) {
+            assert.ok(
+                err.message.includes(
+                    "Traffic of all variations must add up to 100%",
+                ),
+            );
+        }
+    });
+
+    it("cannot add an experiment with inconsistent variables", async () => {
+        let projects = await projectController.getProjectsByOwner(ownerId);
+
+        try {
+            await experimentController.addExperiment(
+                ownerId,
+                mockExperiment(projects[0]["_id"], undefined, undefined, [
+                    {
+                        variationName: "variation2",
+                        normalizedTrafficAmount: 0.4,
+                        variables: [
+                            {
+                                variableName: "var2",
+                                variableType: "String",
+                                variableValue: "test",
+                            },
+                            {
+                                variableName: "var1",
+                                variableType: "String",
+                                variableValue: "test",
+                            },
+                        ],
+                    },
+                    {
+                        variationName: "variation1",
+                        normalizedTrafficAmount: 0.6,
+                        variables: [
+                            {
+                                variableName: "var2",
+                                variableType: "String",
+                                variableValue: "test",
+                            },
+                            {
+                                variableName: "var3",
+                                variableType: "String",
+                                variableValue: "test",
+                            },
+                        ],
+                    },
+                ]),
+            );
+            assert.fail();
+        } catch (err) {
+            assert.ok(
+                err.message.includes(
+                    "Variables names and types must be the same accross all variations",
+                ),
+            );
+        }
+    });
+
+    it("cannot add an experiment with different amount of variables", async () => {
+        let projects = await projectController.getProjectsByOwner(ownerId);
+
+        try {
+            await experimentController.addExperiment(
+                ownerId,
+                mockExperiment(projects[0]["_id"], undefined, undefined, [
+                    {
+                        variationName: "variation2",
+                        normalizedTrafficAmount: 0.4,
+                        variables: [
+                            {
+                                variableName: "var2",
+                                variableType: "String",
+                                variableValue: "test",
+                            },
+                        ],
+                    },
+                    {
+                        variationName: "variation1",
+                        normalizedTrafficAmount: 0.6,
+                        variables: [
+                            {
+                                variableName: "var2",
+                                variableType: "String",
+                                variableValue: "test",
+                            },
+                            {
+                                variableName: "var3",
+                                variableType: "String",
+                                variableValue: "test",
+                            },
+                        ],
+                    },
+                ]),
+            );
+            assert.fail();
+        } catch (err) {
+            assert.ok(
+                err.message.includes(
+                    "Variables names and types must be the same accross all variations",
+                ),
+            );
+        }
+    });
+
+    it("0 variables are not allowed", async () => {
+        let projects = await projectController.getProjectsByOwner(ownerId);
+        try {
+            await experimentController.addExperiment(
+                ownerId,
+                mockExperiment(projects[0]["_id"], undefined, undefined, [
+                    {
+                        variationName: "variation2",
+                        normalizedTrafficAmount: 0.4,
+                        variables: [],
+                    },
+                    {
+                        variationName: "variation1",
+                        normalizedTrafficAmount: 0.6,
+                        variables: [],
+                    },
+                ]),
+            );
+        } catch (err) {
+            assert.ok(err.message.includes("There must be atleast 1 variable"));
+        }
+    });
+});
+
+const mockExperiment = (projectId, startTime, endTime, variations) => {
+    return {
+        _id: {
+            projectId: projectId,
+            experimentName: "exp2",
+        },
+        ...(!variations && {
+            variations: [
+                {
+                    variationName: "variation1",
+                    normalizedTrafficAmount: 0.1,
+                    variables: [
+                        {
+                            variableName: "var2",
+                            variableType: "String",
+                            variableValue: "test",
+                        },
+                        {
+                            variableName: "var1",
+                            variableType: "String",
+                            variableValue: "test",
+                        },
+                    ],
+                },
+                {
+                    variationName: "variation2",
+                    normalizedTrafficAmount: 0.9,
+                    variables: [
+                        {
+                            variableName: "var1",
+                            variableType: "String",
+                            variableValue: "test",
+                        },
+                        {
+                            variableName: "var2",
+                            variableType: "String",
+                            variableValue: "test",
+                        },
+                    ],
+                },
+            ],
+        }),
+        ...(variations && { variations }),
+        ...(startTime && { startTime }),
+        ...(endTime && { endTime }),
+    };
+};
